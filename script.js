800e02022bdd482974b2a2bd99a4ef5ed69d6d75
gmcr <gmcrinn@gmail.com>
2019-05-03 13:52:15 +0500

    v2

--- script.js
+++ script.js
@@ -1,37 +1,15 @@
-   const obj = {
-    "a": 1,
-    "b" :2,
-    "c" :3,
-    "d": 4,
-   "e": 5,
-   "f": 6,
-   "g": 7,
-   "h": 8
-};
+// Code goes here
 
-const arr=["","a","b","c","d","e","f","g","h"];
+const fn=function (interval) {
+        interval = interval || 40;
+        var j = 0;
+        setInterval(function () {
+            var els = document.querySelectorAll(".planet2d");
+            j--;
+            for (var i = 0; i < els.length; i++) {
+                els[i].style.backgroundPosition = j + "px 0px";
+            }
+        }, interval);
+    };
 
-let arr1=[];
-
-
-const fnPUsh=(str,num)=>{
-  
-  if(str<=8&&str>0&&num>0&&num<=8){
-    arr1.push(arr[str]+num);//map.get(str)
-  }//if
-};
-
-const turn=(str0,num0)=>{
-  
-  let str1=obj[str0];
-  arr1=[];
-  
-  fnPUsh(+str1-1,num0-2);
-  fnPUsh(+str1-2,num0-1);
-  fnPUsh(+str1-2,num0+1);
-  fnPUsh(+str1+1,num0-2);
-  fnPUsh(+str1-1,num0+2);
-  fnPUsh(+str1+2,num0-1);
-  fnPUsh(+str1+1,num0+2);
-  fnPUsh(+str1+2,num0+1);
-};
+fn();
